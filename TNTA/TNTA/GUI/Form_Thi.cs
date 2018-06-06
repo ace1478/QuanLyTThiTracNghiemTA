cf88724a5d2c147017dd396af5a9aa91a70fc538
ace1478 <39096733+ace1478@users.noreply.github.com>
2018-06-06 17:39:42 +0700

    Chức năng thi

---
+++ TNTA/TNTA/GUI/Form_Thi.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TNTA.DAO;
+using TNTA.DTO;
+
+namespace TNTA.GUI
+{
+    public partial class Form_Thi : Form
+    {
+        private List<BTDoc> listBTDoc = new List<BTDoc>();
+        private List<BTNguPhap> listBTNguPhap = new List<BTNguPhap>();
+        private List<Panel> ListPl = new List<Panel>();
+        private List<List<CauHoiDoc>> ListCauHoiDoc = new List<List<CauHoiDoc>>();
+        private List<RadioButton> ListRB_Main = new List<RadioButton>();
+        private int STT_DoanVan = -1;
+        private int SoThuTu = 0;
+        private int sl;
+        private int gio = 1;
+        private int phut = 0;
+        private int giay = 0;
+        private int TongGiay = 3600;
+        private int SoCauDung = 0;
+        private int Id_Made;
+
+        public int MSVKT { get; set; }
+        public int id_TV { get; set; }
+        public Form_Thi()
+        {
+            InitializeComponent();
+          
+
+        }
+
+        private void Form_Thi_Load(object sender, EventArgs e)
+        {
+            Id_Made = SinhVien_KyThiDAO.GetMaDe(MSVKT);
+            listBTDoc = BTDocDAO.LoadDSCauHoi_TheoMaDe(Id_Made);
+            listBTNguPhap = BTNguPhapDAO.LoadDSCauHoi_TheoMaDe(Id_Made);
+            foreach (BTDoc item in listBTDoc)
+            {
+                List<CauHoiDoc> rb = CauHoiDocDAO.LoadDSCauHoiDoc(item.Id_Doc);
+                ListCauHoiDoc.Add(rb);
+            }
+            SinhVien sv = ThanhVienDAO.GetThongTinSV(MSVKT);
+            lb_name.Text = sv.HoVaTen;
+            lb_MSV.Text = sv.MaSV;
+            TTKT tk = TTKTDAO.GetTTKT(MSVKT);
+           
+            lb_NamKT.Text = tk.TenKyThi;
+            timer1.Interval = 1000;
+            timer1.Start();
+             sl = listBTNguPhap.Count;
+            foreach (List<CauHoiDoc> item in ListCauHoiDoc)
+            {
+                sl = sl + item.Count;
+            }
+
+            for (int i = 0; i < sl; i++)
+            {
+                TaoListBTT(5, 50 * i, i);
+            }
+            ThemCode(0);
+            btn_TC.Hide();
+
+
+        }
+        public void ThemCode(int TS)
+        {
+            pl_CauHoi.Controls.Clear();
+            Label btm = new Label() { Name = "lb_Stt", Location = new Point(10, 28), Text = "Câu" + (TS + 1), AutoSize = true };
+            Label btm1 = new Label() { Name = "lb_CauHoi", Location = new Point(51, 28), Text = listBTNguPhap[TS].CauHoi, AutoSize = true };
+            Label btm2 = new Label() { Name = "lb_DapAnA", Location = new Point(10, 66), Text = "A:    " + listBTNguPhap[TS].DapAn1, AutoSize = true };
+            Label btm3 = new Label() { Name = "lb_DapAnB", Location = new Point(10, 91), Text = "B:    " + listBTNguPhap[TS].DapAn2, AutoSize = true };
+            Label btm4 = new Label() { Name = "lb_DapAnC", Location = new Point(10, 115), Text = "C:    " + listBTNguPhap[TS].DapAn3, AutoSize = true };
+            Label btm5 = new Label() { Name = "lb_DapAnD", Location = new Point(10, 144), Text = "D:    " + listBTNguPhap[TS].DapAn4, AutoSize = true };
+            pl_CauHoi.Controls.Add(btm); pl_CauHoi.Controls.Add(btm1); pl_CauHoi.Controls.Add(btm2); pl_CauHoi.Controls.Add(btm3); pl_CauHoi.Controls.Add(btm4);
+            pl_CauHoi.Controls.Add(btm5);
+        }
+        private void ThemCode_DoanVan(int id, int STT)
+        {
+            pl_CauHoi.Controls.Clear();
+            Label btm = new Label() { Name = "lb_Stt", Location = new Point(3, 7), Text = "Đoạn Văn", AutoSize = true };
+            RichTextBox rb = new RichTextBox() { Text = listBTDoc[id].DoanVan, Location = new Point(55, 7),Height=140,Width=720,Enabled=false };
+            pl_CauHoi.Controls.Add(rb);
+            pl_CauHoi.Controls.Add(btm);
+            int i = 0;
+            foreach (CauHoiDoc item in ListCauHoiDoc[id])
+            {
+                Label btm1 = new Label() { Location = new Point(3, 170 + i * 30), Text = "" + (STT+1), AutoSize = true };
+                Label btm2 = new Label() { Name = "lb_DapAnA", Location = new Point(64, 170 + i * 30), Text = "A: " + item.DapAn1, AutoSize = true };
+                Label btm3 = new Label() { Name = "lb_DapAnB", Location = new Point(252, 170 + i * 30), Text = "B: " + item.DapAn2, AutoSize = true };
+                Label btm4 = new Label() { Name = "lb_DapAnC", Location = new Point(458, 170 + i * 30), Text = "C: " + item.DapAn3, AutoSize = true };
+                Label btm5 = new Label() { Name = "lb_DapAnD", Location = new Point(643, 170 + i * 30), Text = "D: " + item.DapAn4, AutoSize = true };
+                pl_CauHoi.Controls.Add(btm1); pl_CauHoi.Controls.Add(btm2); pl_CauHoi.Controls.Add(btm3); pl_CauHoi.Controls.Add(btm4);
+                pl_CauHoi.Controls.Add(btm5);
+                i++; STT++;
+            }
+
+        }
+        private void TaoListBTT(int x, int y, int stt)
+        {
+            Panel pl = new Panel() { Name = "Pl" + stt, Location = new Point(x, y) };
+            pl.Width = 200; pl.Height = 50;
+            ListPl.Add(pl);
+
+            Pl_CauTraLoi.Controls.Add(pl);
+
+            Label lb = new Label() { Text = "Câu" + (stt + 1), Location = new Point(3, 15) };
+            RadioButton rb1 = new RadioButton() { Text = "A", Name = "1", Location = new Point(50, 10), Tag = stt };
+            RadioButton rb2 = new RadioButton() { Text = "B", Name = "2", Location = new Point(90, 10), Tag = stt };
+            RadioButton rb3 = new RadioButton() { Text = "C", Name = "3", Location = new Point(130, 10), Tag = stt };
+            RadioButton rb4 = new RadioButton() { Text = "D", Name = "4", Location = new Point(170, 10), Tag = stt };
+
+            ListRB_Main.Add(rb1); ListRB_Main.Add(rb2); ListRB_Main.Add(rb3); ListRB_Main.Add(rb4);
+            pl.Controls.Add(rb4);
+            pl.Controls.Add(rb3);
+            pl.Controls.Add(rb2);
+            pl.Controls.Add(rb1);
+            pl.Controls.Add(lb);
+
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_NB_Click(object sender, EventArgs e)
+        {
+            List<List<string>> list_DapAn = List_DapAnCauHoi();
+             List<string> listDapAnDung = GetListDapAnDung();
+            foreach (RadioButton item in ListRB_Main)
+            {
+                if(item.Checked==true)
+                {
+                    int CauSo = int.Parse(item.Tag.ToString());
+                    if (list_DapAn[CauSo][int.Parse(item.Name.ToString())-1] == listDapAnDung[CauSo]) SoCauDung++;
+                    
+                    
+                }
+            }
+            MessageBox.Show("Số điểm của bạn là :" + SoCauDung, "Thông Báo");
+            string querry = "Update Sv_KyThi set TongDiem=" + SoCauDung + "where MaSVKT=" + MSVKT;
+            DataProvider.instanse.ExecuteNonquerry(querry);
+            timer1.Stop();
+
+            btn_NB.Enabled = false;
+            btn_Back.Enabled = false;
+            btn_Next.Enabled = false;
+            btn_TC.Show();
+        }
+
+
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            SoThuTu++;
+
+            if (SoThuTu >= listBTNguPhap.Count)
+            {
+                
+                if (STT_DoanVan >= listBTDoc.Count) { STT_DoanVan = listBTDoc.Count-1; }
+                else
+                {
+                    if(SoThuTu>sl) { ThemCode_DoanVan(STT_DoanVan, SoThuTu);SoThuTu = SoThuTu - ListCauHoiDoc[STT_DoanVan].Count; }
+                    if (STT_DoanVan == -1) STT_DoanVan = 0;
+                    ThemCode_DoanVan(STT_DoanVan, SoThuTu);
+                    if(STT_DoanVan!=listBTDoc.Count-1) SoThuTu = SoThuTu + ListCauHoiDoc[STT_DoanVan].Count - 1;
+                    else SoThuTu = SoThuTu + ListCauHoiDoc[STT_DoanVan].Count ;
+                    if (SoThuTu > sl) SoThuTu = sl;
+                    
+                }
+                STT_DoanVan++;
+                if (STT_DoanVan >= listBTDoc.Count) { STT_DoanVan = listBTDoc.Count-1 ; }
+            }
+            else
+            {
+                ThemCode(SoThuTu);
+                
+            }
+           
+
+        }
+
+        private string ConvertS_To_H(int s)
+        {
+            string Gio;
+            string Phut;
+            string Giay;
+            gio = s / 3600;
+            phut = (s - gio * 3600) / 60;
+            giay = s - gio * 3600 - phut * 60;
+            if (gio >= 10) { Gio = gio.ToString(); }
+            else { Gio = "0" + gio; }
+            if (phut >= 10) { Phut = phut.ToString(); }
+            else { Phut = "0" + phut; }
+            if (giay >= 10) { Giay = giay.ToString(); }
+            else { Giay = "0" + giay; }
+            string st = Gio + ":" + Phut + ":" + Giay;
+            return st;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            TongGiay--;
+            string text = ConvertS_To_H(TongGiay);
+            lb_Time.Text = text;
+            if (TongGiay == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Bạn đã hết giơ làm bài", "Thông báo");
+
+            }
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            if (SoThuTu != 0)
+            { 
+                if(STT_DoanVan>=0)
+                {
+                    //if(STT_DoanVan>=1) SoThuTu = SoThuTu - ListCauHoiDoc[STT_DoanVan].Count- ListCauHoiDoc[STT_DoanVan-1].Count;
+                    //else SoThuTu = SoThuTu - ListCauHoiDoc[STT_DoanVan].Count;
+                    //STT_DoanVan--;
+                    STT_DoanVan--;
+                    int j = 0;
+                    for (int i=0;i<STT_DoanVan;i++)
+                    {
+                        j =j+ ListCauHoiDoc[i].Count();
+                    }
+                    SoThuTu = listBTNguPhap.Count + j;
+                   
+
+                }
+                    
+                else
+                {
+                    SoThuTu--;
+                }
+               
+
+                if (STT_DoanVan >= 0) { ThemCode_DoanVan(STT_DoanVan, SoThuTu);SoThuTu = SoThuTu + ListCauHoiDoc[STT_DoanVan].Count; }
+
+                else
+                {
+                    if(SoThuTu==listBTNguPhap.Count) SoThuTu--;
+
+                    ThemCode(SoThuTu);
+                }
+            }
+
+        }
+        private List<string> GetListDapAnDung()
+        {
+            List<string> list = new List<string>();
+            foreach(BTNguPhap item in listBTNguPhap)
+            {
+                string st = item.DapAnDung;
+                list.Add(st);
+            }
+            foreach(List<CauHoiDoc> item in ListCauHoiDoc)
+            {
+                foreach (CauHoiDoc item1 in item)
+                {
+                    string st = item1.DapAnDung;
+                    list.Add(st);
+                }
+            }
+            return list;
+        }
+        private List<List<string>> List_DapAnCauHoi()
+        {
+            List<List<string>> List_Tong= new List<List<string>>();
+            foreach (BTNguPhap item in listBTNguPhap)
+            {
+                List<string> list = new List<string>();
+                list.Add(item.DapAn1);
+                list.Add(item.DapAn2);
+                list.Add(item.DapAn3);
+                list.Add(item.DapAn4);
+                List_Tong.Add(list);
+            }
+            foreach (List<CauHoiDoc> item in ListCauHoiDoc)
+            {
+                foreach (CauHoiDoc item1 in item)
+                {
+                    List<string> list = new List<string>();
+                    list.Add(item1.DapAn1);
+                    list.Add(item1.DapAn2);
+                    list.Add(item1.DapAn3);
+                    list.Add(item1.DapAn4);
+                    List_Tong.Add(list);
+                }
+            }
+            return List_Tong;
+        }
+
+        private void pl_Header_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_TC_Click(object sender, EventArgs e)
+        {
+            Form_CaNhanCuaSV f = new Form_CaNhanCuaSV();
+            f.id_TV = id_TV;
+            f.MSVKT = MSVKT;
+            this.Hide();
+            f.Show();
+        }
+    }
+}
---
+++ TNTA/TNTA/GUI/Form_Thi.designer.cs
@@ -0,0 +1,282 @@
+﻿namespace TNTA.GUI
+{
+    partial class Form_Thi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pl_Header = new System.Windows.Forms.Panel();
+            this.lb_NamKT = new System.Windows.Forms.Label();
+            this.pl_CauHoi = new System.Windows.Forms.Panel();
+            this.Pl_CauTraLoi = new System.Windows.Forms.Panel();
+            this.Pl_Timer = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lb_Time = new System.Windows.Forms.Label();
+            this.btn_NB = new System.Windows.Forms.Button();
+            this.btn_Back = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb_MSV = new System.Windows.Forms.Label();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_TC = new System.Windows.Forms.Button();
+            this.pl_Header.SuspendLayout();
+            this.Pl_Timer.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pl_Header
+            // 
+            this.pl_Header.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pl_Header.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pl_Header.Controls.Add(this.lb_NamKT);
+            this.pl_Header.Location = new System.Drawing.Point(2, 3);
+            this.pl_Header.Name = "pl_Header";
+            this.pl_Header.Size = new System.Drawing.Size(799, 67);
+            this.pl_Header.TabIndex = 0;
+            this.pl_Header.Paint += new System.Windows.Forms.PaintEventHandler(this.pl_Header_Paint);
+            // 
+            // lb_NamKT
+            // 
+            this.lb_NamKT.AutoSize = true;
+            this.lb_NamKT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_NamKT.Location = new System.Drawing.Point(308, 19);
+            this.lb_NamKT.Name = "lb_NamKT";
+            this.lb_NamKT.Size = new System.Drawing.Size(197, 25);
+            this.lb_NamKT.TabIndex = 17;
+            this.lb_NamKT.Text = "Chúc bạn may mắn";
+            // 
+            // pl_CauHoi
+            // 
+            this.pl_CauHoi.AutoScroll = true;
+            this.pl_CauHoi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pl_CauHoi.Location = new System.Drawing.Point(2, 70);
+            this.pl_CauHoi.Name = "pl_CauHoi";
+            this.pl_CauHoi.Size = new System.Drawing.Size(799, 426);
+            this.pl_CauHoi.TabIndex = 1;
+            // 
+            // Pl_CauTraLoi
+            // 
+            this.Pl_CauTraLoi.AutoScroll = true;
+            this.Pl_CauTraLoi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Pl_CauTraLoi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pl_CauTraLoi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Pl_CauTraLoi.Location = new System.Drawing.Point(804, 138);
+            this.Pl_CauTraLoi.Name = "Pl_CauTraLoi";
+            this.Pl_CauTraLoi.Size = new System.Drawing.Size(252, 358);
+            this.Pl_CauTraLoi.TabIndex = 1;
+            // 
+            // Pl_Timer
+            // 
+            this.Pl_Timer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Pl_Timer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pl_Timer.Controls.Add(this.label12);
+            this.Pl_Timer.Controls.Add(this.lb_Time);
+            this.Pl_Timer.Location = new System.Drawing.Point(804, 70);
+            this.Pl_Timer.Name = "Pl_Timer";
+            this.Pl_Timer.Size = new System.Drawing.Size(252, 65);
+            this.Pl_Timer.TabIndex = 2;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(77, 13);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(92, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Thời Gian Còn Lại";
+            // 
+            // lb_Time
+            // 
+            this.lb_Time.AutoSize = true;
+            this.lb_Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Time.ForeColor = System.Drawing.Color.Red;
+            this.lb_Time.Location = new System.Drawing.Point(75, 27);
+            this.lb_Time.Name = "lb_Time";
+            this.lb_Time.Size = new System.Drawing.Size(96, 25);
+            this.lb_Time.TabIndex = 10;
+            this.lb_Time.Text = "00:00:00";
+            // 
+            // btn_NB
+            // 
+            this.btn_NB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_NB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_NB.ForeColor = System.Drawing.Color.White;
+            this.btn_NB.Location = new System.Drawing.Point(939, 499);
+            this.btn_NB.Name = "btn_NB";
+            this.btn_NB.Size = new System.Drawing.Size(117, 44);
+            this.btn_NB.TabIndex = 0;
+            this.btn_NB.Text = "Nộp bài";
+            this.btn_NB.UseVisualStyleBackColor = false;
+            this.btn_NB.Click += new System.EventHandler(this.btn_NB_Click);
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.BackColor = System.Drawing.Color.Purple;
+            this.btn_Back.ForeColor = System.Drawing.Color.White;
+            this.btn_Back.Location = new System.Drawing.Point(5, 501);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(112, 42);
+            this.btn_Back.TabIndex = 4;
+            this.btn_Back.Text = "Back";
+            this.btn_Back.UseVisualStyleBackColor = false;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.BackColor = System.Drawing.Color.Blue;
+            this.btn_Next.ForeColor = System.Drawing.Color.White;
+            this.btn_Next.Location = new System.Drawing.Point(133, 501);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(112, 42);
+            this.btn_Next.TabIndex = 5;
+            this.btn_Next.Text = "Next";
+            this.btn_Next.UseVisualStyleBackColor = false;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lb_MSV);
+            this.panel1.Controls.Add(this.lb_name);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(804, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(252, 67);
+            this.panel1.TabIndex = 17;
+            // 
+            // lb_MSV
+            // 
+            this.lb_MSV.AutoSize = true;
+            this.lb_MSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_MSV.Location = new System.Drawing.Point(85, 33);
+            this.lb_MSV.Name = "lb_MSV";
+            this.lb_MSV.Size = new System.Drawing.Size(67, 20);
+            this.lb_MSV.TabIndex = 19;
+            this.lb_MSV.Text = "Thí sinh:";
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_name.Location = new System.Drawing.Point(85, 5);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(67, 20);
+            this.lb_name.TabIndex = 18;
+            this.lb_name.Text = "Thí sinh:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "MSV    :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Thí sinh:";
+            // 
+            // btn_TC
+            // 
+            this.btn_TC.BackColor = System.Drawing.Color.Olive;
+            this.btn_TC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TC.ForeColor = System.Drawing.Color.White;
+            this.btn_TC.Location = new System.Drawing.Point(821, 499);
+            this.btn_TC.Name = "btn_TC";
+            this.btn_TC.Size = new System.Drawing.Size(117, 44);
+            this.btn_TC.TabIndex = 18;
+            this.btn_TC.Text = "Back";
+            this.btn_TC.UseVisualStyleBackColor = false;
+            this.btn_TC.Click += new System.EventHandler(this.btn_TC_Click);
+            // 
+            // Form_Thi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1057, 543);
+            this.Controls.Add(this.btn_TC);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.btn_Back);
+            this.Controls.Add(this.btn_NB);
+            this.Controls.Add(this.Pl_Timer);
+            this.Controls.Add(this.Pl_CauTraLoi);
+            this.Controls.Add(this.pl_CauHoi);
+            this.Controls.Add(this.pl_Header);
+            this.Name = "Form_Thi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_Thi";
+            this.Load += new System.EventHandler(this.Form_Thi_Load);
+            this.pl_Header.ResumeLayout(false);
+            this.pl_Header.PerformLayout();
+            this.Pl_Timer.ResumeLayout(false);
+            this.Pl_Timer.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pl_Header;
+        private System.Windows.Forms.Panel pl_CauHoi;
+        private System.Windows.Forms.Panel Pl_CauTraLoi;
+        private System.Windows.Forms.Panel Pl_Timer;
+        private System.Windows.Forms.Button btn_NB;
+        private System.Windows.Forms.Button btn_Back;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lb_Time;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lb_NamKT;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_MSV;
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Button btn_TC;
+    }
+}
