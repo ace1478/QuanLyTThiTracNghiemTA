76998202cadddb13815751319b0f0ca9a5a0548f
trantuananh1310 <39252159+trantuananh1310@users.noreply.github.com>
2018-06-06 22:33:02 +0700

    Form_Quyen

---
+++ TNTA/TNTA/GUI/Form_Quyen.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TNTA.DAO;
+
+namespace TNTA.GUI
+{
+    public partial class Form_Quyen : Form
+    {
+        private int TrangThai = -1;
+        public Form_Quyen()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Quyen_Load(object sender, EventArgs e)
+        {
+            LoadForm();
+        }
+        private void LoadForm()
+        {
+            string querry = "Select * from QuyenSD";
+            DataTable Tb = DataProvider.instanse.Executequerry(querry);
+            dtgv_QuyenSD.DataSource = Tb;
+            lb_ThongBao.Hide();
+            txb_TenQuyen.Text = "";
+            txb_TenQuyen.Enabled = false;
+            btn_Add.Enabled = true;
+            btn_Edit.Enabled = false;
+            btn_Delete.Enabled = false;
+            btn_Save.Enabled = false;
+            btn_Huy.Enabled = false;
+        }
+
+        private void dtgv_QuyenSD_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txb_TenQuyen.Text = dtgv_QuyenSD.CurrentRow.Cells["TenQuyen"].Value.ToString();
+            btn_Edit.Enabled = true;
+            btn_Delete.Enabled = true;
+       
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            txb_TenQuyen.Text = "";
+            txb_TenQuyen.Enabled = true;
+           btn_Add.Enabled = false;
+            btn_Edit.Enabled = false;
+            btn_Delete.Enabled = false;
+            btn_Save.Enabled = true;
+            btn_Huy.Enabled = true;
+            TrangThai = 0;
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if (TrangThai == 0)// Trạng thái thêm mới
+            {
+                if (txb_TenQuyen.Text != "")
+                {
+
+                    string querry = " Insert into QuyenSD  values(N'" + txb_TenQuyen.Text + "')";
+                    DataProvider.instanse.ExecuteNonquerry(querry);
+                    MessageBox.Show("Thêm dữ liệu thành công");
+                    LoadForm();
+                }
+                else
+                {
+                    lb_ThongBao.Text = "Bạn chưa nhập thông tin ";
+                    lb_ThongBao.Show();
+                }
+            }
+            else if (TrangThai == 1)//Trạng thái sửa
+            {
+                if (txb_TenQuyen.Text != "")
+                {
+                    string querry = "update QuyenSD set TenQuyen = N'" + txb_TenQuyen.Text + "' where Id_QuyenSD =" + dtgv_QuyenSD.CurrentRow.Cells["Id_QuyenSD"].Value.ToString();
+                    DataProvider.instanse.ExecuteNonquerry(querry);
+                    MessageBox.Show("Sửa dữ liệu thành công");
+                    LoadForm();
+                }
+                else
+                {
+                    lb_ThongBao.Text = "Bạn chưa nhập thông tin ";
+                    lb_ThongBao.Show();
+                }
+
+            }  
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Bạn có muốn hủy thao tác thêm không ?", "Thông báo ", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    string querry = "Delete QuyenSD where Id_QuyenSD=" + dtgv_QuyenSD.CurrentRow.Cells["Id_QuyenSD"].Value.ToString();
+                    DataProvider.instanse.ExecuteNonquerry(querry);
+                    MessageBox.Show("Đã xóa dữ liệu thành công!");
+                    LoadForm();
+                }
+              
+            }
+           catch
+            {
+
+            }
+
+        }
+
+        private void btn_Edit_Click(object sender, EventArgs e)
+        {
+            TrangThai = 1;
+           
+            txb_TenQuyen.Enabled = true;
+            btn_Add.Enabled = false;
+            btn_Edit.Enabled = false;
+            btn_Delete.Enabled = false;
+            btn_Save.Enabled = true;
+            btn_Huy.Enabled = true;
+        }
+
+        private void btn_Huy_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn hủy thao tác thêm không ?", "Thông báo ", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                LoadForm();
+            }
+        }
+    }
+}
---
+++ TNTA/TNTA/GUI/Form_Quyen.designer.cs
@@ -0,0 +1,199 @@
+﻿namespace TNTA.GUI
+{
+    partial class Form_Quyen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgv_QuyenSD = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Huy = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.txb_TenQuyen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_ThongBao = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_QuyenSD)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtgv_QuyenSD
+            // 
+            this.dtgv_QuyenSD.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgv_QuyenSD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv_QuyenSD.Location = new System.Drawing.Point(11, 16);
+            this.dtgv_QuyenSD.Name = "dtgv_QuyenSD";
+            this.dtgv_QuyenSD.Size = new System.Drawing.Size(475, 499);
+            this.dtgv_QuyenSD.TabIndex = 0;
+            this.dtgv_QuyenSD.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgv_QuyenSD_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.dtgv_QuyenSD);
+            this.groupBox1.Location = new System.Drawing.Point(1, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(495, 524);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lb_ThongBao);
+            this.groupBox2.Controls.Add(this.btn_Delete);
+            this.groupBox2.Controls.Add(this.btn_Huy);
+            this.groupBox2.Controls.Add(this.btn_Save);
+            this.groupBox2.Controls.Add(this.btn_Edit);
+            this.groupBox2.Controls.Add(this.btn_Add);
+            this.groupBox2.Controls.Add(this.txb_TenQuyen);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(502, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(463, 513);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin chi tiết";
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Enabled = false;
+            this.btn_Delete.Location = new System.Drawing.Point(370, 192);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 39);
+            this.btn_Delete.TabIndex = 6;
+            this.btn_Delete.Text = "Xóa";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Huy
+            // 
+            this.btn_Huy.Enabled = false;
+            this.btn_Huy.Location = new System.Drawing.Point(282, 190);
+            this.btn_Huy.Name = "btn_Huy";
+            this.btn_Huy.Size = new System.Drawing.Size(75, 39);
+            this.btn_Huy.TabIndex = 5;
+            this.btn_Huy.Text = "Hủy";
+            this.btn_Huy.UseVisualStyleBackColor = true;
+            this.btn_Huy.Click += new System.EventHandler(this.btn_Huy_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Enabled = false;
+            this.btn_Save.Location = new System.Drawing.Point(191, 190);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 39);
+            this.btn_Save.TabIndex = 4;
+            this.btn_Save.Text = "Lưu";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Enabled = false;
+            this.btn_Edit.Location = new System.Drawing.Point(98, 191);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(75, 39);
+            this.btn_Edit.TabIndex = 3;
+            this.btn_Edit.Text = "Sửa";
+            this.btn_Edit.UseVisualStyleBackColor = true;
+            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(10, 190);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 39);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Thêm";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // txb_TenQuyen
+            // 
+            this.txb_TenQuyen.Enabled = false;
+            this.txb_TenQuyen.Location = new System.Drawing.Point(133, 104);
+            this.txb_TenQuyen.Name = "txb_TenQuyen";
+            this.txb_TenQuyen.Size = new System.Drawing.Size(256, 20);
+            this.txb_TenQuyen.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Quyền";
+            // 
+            // lb_ThongBao
+            // 
+            this.lb_ThongBao.AutoSize = true;
+            this.lb_ThongBao.ForeColor = System.Drawing.Color.Red;
+            this.lb_ThongBao.Location = new System.Drawing.Point(57, 148);
+            this.lb_ThongBao.Name = "lb_ThongBao";
+            this.lb_ThongBao.Size = new System.Drawing.Size(60, 13);
+            this.lb_ThongBao.TabIndex = 7;
+            this.lb_ThongBao.Text = "Tên Quyền";
+            // 
+            // Form_Quyen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(989, 527);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form_Quyen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_Quyen";
+            this.Load += new System.EventHandler(this.Form_Quyen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_QuyenSD)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgv_QuyenSD;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Huy;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Edit;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.TextBox txb_TenQuyen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_ThongBao;
+    }
+}
